a6719f043f327201998931fa855078da9789c4a7
Morten Høfft <mhoefft@gbif.org>
2022-02-03 09:55:11 +0100

    increasd fontsize and new lit config

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,22 +1,46 @@
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary
-}});
+var siteTheme = gbifReactComponents.themeBuilder.extend({
+  baseTheme: 'light', extendWith: {
+    primary: themeStyle.colors.primary,
+    fontSize: '16px'
+  }
+});
 
 var siteConfig = {
-  rootPredicate: {
-    "type": "and",
-    "predicates": [
-   		{
-        "type": "in",
-        "key": "country",
-        "values": ["US", "UM", "AS", "FM", "GU", "MH", "MP", "PR", "PW", "VI"]
-      },
-      {
-        "type": "equals",
-        "key": "notIssues",
-        "value": "COUNTRY_COORDINATE_MISMATCH"
-      }
-    ]
+  occurrence: {
+    rootPredicate: {
+      "type": "and",
+      "predicates": [
+        {
+          "type": "in",
+          "key": "country",
+          "values": ["US", "UM", "AS", "FM", "GU", "MH", "MP", "PR", "PW", "VI"]
+        },
+        {
+          "type": "equals",
+          "key": "notIssues",
+          "value": "COUNTRY_COORDINATE_MISMATCH"
+        }
+      ]
+    },
+    highlightedFilters: ['taxonKey', 'occurrenceStatus', 'gadmGid', 'year', 'datasetName', 'occurrenceIssue', 'datasetKey']
   },
-  highlightedFilters: ['taxonKey','occurrenceStatus', 'gadmGid', 'year', 'datasetName', 'occurrenceIssue', 'datasetKey']
+  literature: {
+    rootFilter: {
+      predicate: {
+        type: 'or', predicates: [
+          {
+            type: 'in',
+            key: 'countriesOfResearcher',
+            values: ['US', 'UM', 'AS', 'FM', 'GU', 'MH', 'MP', 'PR', 'PW', 'VI']
+          },
+          {
+            type: 'in',
+            key: 'countriesOfCoverage',
+            values: ['US', 'UM', 'AS', 'FM', 'GU', 'MH', 'MP', 'PR', 'PW', 'VI']
+          }
+        ]
+      }
+    },
+    highlightedFilters: ['q', 'countriesOfResearcher', 'countriesOfCoverage', 'year']
+  }
 };
